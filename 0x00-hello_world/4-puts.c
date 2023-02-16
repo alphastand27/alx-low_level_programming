362b4e50cdb25c2ace7291145a6e1fdfcc8b4f12
micheal <micksilwimba867@gmail.com>
2023-02-16 07:54:28 -0800

    simple c program using puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
-int main(void){
-char p[] = "Programming is like building a multilingual puzzle";
-
-puts(p);
-
-return 0;
+/*entry point of program*/
+int main(void)
+{
+	/*string initialization*/
+	char p[] = "\"Programming is like building a multilingual puzzle\n";
+	
+	puts(p);
+	
+	return (0);
 }

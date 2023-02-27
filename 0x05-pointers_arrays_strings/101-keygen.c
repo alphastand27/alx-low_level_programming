50723f40b08bcfccdad37dc1ec00b694842bacb2
micheal <micksilwimba867@gmail.com>
2023-02-27 08:23:39 -0800

    random valid password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,13 +6,13 @@
 
 int main(void)
 {
-    srand(time(NULL)); // initialize the random number generator with the current time
+srand(time(NULL)); // initialize the random number generator with the current time
     
-    char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
+char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
     
-    for (int i = 0; i < PASSWORD_LENGTH; i++) {
-        int rand_num = rand() % 62; // generate a random number between 0 and 61
-        
+for (int i = 0; i < PASSWORD_LENGTH; i++) {
+	int rand_num = rand() % 62; // generate a random number between 0 and 61
+
         if (rand_num < 26) { // if it's between 0 and 25, add a lowercase letter
             password[i] = 'a' + rand_num;
         } else if (rand_num < 52) { // if it's between 26 and 51, add an uppercase letter
@@ -20,12 +20,12 @@ int main(void)
         } else { // if it's between 52 and 61, add a digit
             password[i] = '0' + rand_num - 52;
         }
-    }
+}
     
-    password[PASSWORD_LENGTH] = '\0'; // add the null terminator
+password[PASSWORD_LENGTH] = '\0'; // add the null terminator
     
-    printf("%s", password); // print the password
+printf("%s", password); // print the password
     
-    return 0;
+return 0;
 }
 

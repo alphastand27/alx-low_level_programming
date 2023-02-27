59b65719850585fb14e784d888e79d51b77c43a4
micheal <micksilwimba867@gmail.com>
2023-02-27 08:20:41 -0800

    random valid password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,31 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 10
+#define PASSWORD_LENGTH 60
 
-/**
- * main - entry point for program
- *
- * Return: 0 on success
- */
 int main(void)
 {
-char password[PASSWORD_LENGTH + 1];
-const char *valid_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-int i;
-
-srand(time(NULL)); /* Seed the random number generator */
-
-/* Generate a random password */
-for (i = 0; i < PASSWORD_LENGTH; i++)
-{
-        password[i] = valid_chars[rand() % 36];
-}
-
-password[i] = '\0'; /* Add null terminator to end of password */
-
-printf("Random password: %s\n", password);
-
-return 0;
+    srand(time(NULL)); // initialize the random number generator with the current time
+    
+    char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
+    
+    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+        int rand_num = rand() % 62; // generate a random number between 0 and 61
+        
+        if (rand_num < 26) { // if it's between 0 and 25, add a lowercase letter
+            password[i] = 'a' + rand_num;
+        } else if (rand_num < 52) { // if it's between 26 and 51, add an uppercase letter
+            password[i] = 'A' + rand_num - 26;
+        } else { // if it's between 52 and 61, add a digit
+            password[i] = '0' + rand_num - 52;
+        }
+    }
+    
+    password[PASSWORD_LENGTH] = '\0'; // add the null terminator
+    
+    printf("%s", password); // print the password
+    
+    return 0;
 }
 

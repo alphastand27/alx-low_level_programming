5abe39e2683c8fb44c1fa11a7dddf850046c4a7d
micheal <micksilwimba867@gmail.com>
2023-02-27 08:27:48 -0800

    random valid password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,26 +6,21 @@
 
 int main(void)
 {
-srand(time(NULL)); // initialize the random number generator with the current time
+    srand(time(NULL)); // initialize the random number generator with the current time
     
-char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
+    char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
     
-for (int i = 0; i < PASSWORD_LENGTH; i++) {
-	int rand_num = rand() % 62; // generate a random number between 0 and 61
-
-        if (rand_num < 26) { // if it's between 0 and 25, add a lowercase letter
-            password[i] = 'a' + rand_num;
-        } else if (rand_num < 52) { // if it's between 26 and 51, add an uppercase letter
-            password[i] = 'A' + rand_num - 26;
-        } else { // if it's between 52 and 61, add a digit
-            password[i] = '0' + rand_num - 52;
-        }
-}
+    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+        int rand_num = rand() % 94 + 33; // generate a random number between 33 and 126
+        
+        password[i] = (char)rand_num; // convert the random number to a character
+    }
     
-password[PASSWORD_LENGTH] = '\0'; // add the null terminator
+    password[PASSWORD_LENGTH] = '\0'; // add the null terminator
     
-printf("%s", password); // print the password
+    printf("%s\n", password); // print the password followed by a newline character
+    printf("Tada! Congrats\n"); // print the success message
     
-return 0;
+    return 0;
 }
 

f36484ccd7b55ac8399e7443cef928401c0dd12f
micheal <micksilwimba867@gmail.com>
2023-03-14 08:08:45 -0700

    working with malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,23 +10,23 @@
  */
 int **alloc_grid(int width, int height)
 {
-int **array, i = 0, j = 0;
+	int **array, i = 0, j = 0;
 
-if (width <= 0 || height <= 0)
-	return (NULL);
-
-array = (int **)malloc(sizeof(int *) * height);
-if (array == NULL)
-	return (NULL);
-for (; i < height; i++)
-{
-	array[i] = (int *)malloc(sizeof(int) * width);
-	if (array[i] == NULL)
+	if (width <= 0 || height <= 0)
 		return (NULL);
-}
 
-for (i = 0; i < height; i++)
-	for (; j < width; j++)
-		array[i][j] = 0;
-return (array);
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return (NULL);
+	for (; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+			return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+		for (; j < width; j++)
+			array[i][j] = 0;
+	return (array);
 }

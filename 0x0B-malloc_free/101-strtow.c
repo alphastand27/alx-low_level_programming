60e6def74e08ccc572afe90f86b4c2d3c8f1f738
micheal <micksilwimba867@gmail.com>
2023-03-14 08:48:51 -0700

    working with malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,49 +2,64 @@
 #include <stdlib.h>
 
 /**
- * strtow - A function that splits a string into words
- * @str: An input pointer of the string to split
- * Return: Apointer to concatened strings or NULL if it str is NULL
+ * strtow - Splits a string into words
+ * @str: The string to split
+ *
+ * Return: Pointer to an array of strings (words)
+ *         or NULL if str == NULL or str == ""
  */
 char **strtow(char *str)
 {
-	char **array;
-	int i = 0, j, m, k = 0, len = 0, count = 0;
+char **words;
+int i, j, k, len, wc;
 
-	if (str == NULL || *str == '\0')
-		return (NULL);
-	for (; str[i]; i++)
-	{
-		if ((str[i] != ' ' || *str != '\t') &&
-				((str[i + 1] == ' ' || str[i + 1] == '\t') || str[i + 1] == '\n'))
-			count++;
-	}
-	if (count == 0)
-		return (NULL);
-	array = malloc(sizeof(char *) * (count + 1));
-	if (array == NULL)
-		return (NULL);
-	for (i = 0; str[i] != '\0' && k < count; i++)
-	{
-		if (str[i] != ' ' || str[i] != '\t')
-		{
-			len = 0;
-			j = i;
-			while ((str[j] != ' ' || str[j] != '\t') && str[j] != '\0')
-				j++, len++;
-			array[k] = malloc((len + 1) * sizeof(char));
-			if (array[k] == NULL)
-			{
-				for (k = k - 1; k >= 0; k++)
-					free(array[k]);
-				free(array);
-				return (NULL);
-			}
-			for (m = 0; m < len; m++, i++)
-				array[k][m] = str[i];
-			array[k++][m] = '\0';
-		}
-	}
-	array[k] = NULL;
-	return (array);
+if (str == NULL || *str == '\0')
+        return (NULL);
+
+wc = 0;
+len = 0;
+while (str[len])
+{
+        if ((str[len] != ' ' && str[len] != '\t') &&
+              (str[len + 1] == ' ' || str[len + 1] == '\t' ||
+              str[len + 1] == '\n' || str[len + 1] == '\0'))
+                wc++;
+        len++;
 }
+
+words = malloc(sizeof(char *) * (wc + 1));
+if (words == NULL)
+        return (NULL);
+
+i = 0;
+while (*str)
+{
+        if (*str != ' ' && *str != '\t' && *str != '\n')
+        {
+                j = 0;
+                while (str[j] != ' ' && str[j] != '\t' &&
+                        str[j] != '\n' && str[j] != '\0')
+                        j++;
+
+                words[i] = malloc(sizeof(char) * (j + 1));
+                if (words[i] == NULL)
+                {
+                        while (i-- >= 0)
+                                free(words[i]);
+                        free(words);
+                        return (NULL);
+                }
+
+                for (k = 0; k < j; k++)
+                        words[i][k] = *str++;
+
+                words[i][k] = '\0';
+                i++;
+        }
+        else
+                str++;
+}
+words[i] = NULL;
+return (words);
+}
+
